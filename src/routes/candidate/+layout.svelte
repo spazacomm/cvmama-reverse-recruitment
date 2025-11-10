6a3f62271199229aed1935b83629067741126880
spazacomm <spazacomm@gmail.com>
2025-11-10 20:46:18 +0300

    chnages

--- src/routes/candidate/+layout.svelte
+++ src/routes/candidate/+layout.svelte
@@ -501,14 +501,7 @@
                <div class="row">
                    <div class="col-12 text-center">
                        {new Date().getFullYear()} 
-                       &copy; Reback. Crafted by
-                       <iconify-icon
-                           icon="iconamoon:heart-duotone"
-                           class="fs-18 align-middle text-danger"
-                       ></iconify-icon>
-                       <a href="https://1.envato.market/techzaa" class="fw-bold footer-text" target="_blank"
-                           >Techzaa</a
-                       >
+                       &copy; cvmama career services. 
                    </div>
                </div>
            </div>
